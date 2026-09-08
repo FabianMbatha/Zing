de64bb87f6ca396189a4cabf3c8e56e8c8113ed4
Fabian Mbatha <fabianmbatha1204@gmail.com>
2026-09-08 14:25:52 +0300

    feat: Created bottom bar (#3)

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/navbar";
 import Card from "./components/profile_card";
+import BottomNav from "./components/bottom_bar";
 
 export default function Page() {
   return (
     <main>
         <Navbar />
         <Card />
+        <BottomNav />
     </main>
   )
 }
